941c87a6a7e8e04e52620791703b9a822eb3b637
Edukondalu Badugu <edukondalu.badugu@gmail.com>
2018-09-12 11:32:29 +0530

    Initial commit

---
+++ src/main/java/WhitelistValidation.java
@@ -0,0 +1,9 @@
+/**
+ * @author j1008043
+ * @version $Revision: 1.0 $
+ */
+public class WhitelistValidation {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
